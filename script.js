935b5e01c30756dfcfc86a3c120bdbf1fd368506
28sungmin <gooddays4001@naver.com>
2024-04-30 18:21:37 +0900

    online_test finish

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 메인 이미지 넘기기
 var slide = document.querySelector(".slider");
 var no = 1;
 
@@ -6,3 +7,14 @@ setInterval(function () {
   slide.style.backgroundImage = `url(./img/header2_${no}.jpeg)`;
   if (no == 2) no = 0;
 }, 5000);
+
+// 아이콘 넣기
+const diffList = document.querySelectorAll(".diff ul li");
+
+for (let i = 0; i < diffList.length; i++) {
+  const diffItem = diffList[i];
+  const img = document.createElement("img");
+  img.src = `./img/diff_${i + 1}.png`; // 이미지 경로 설정
+  img.alt = "차별점 이미지";
+  diffItem.querySelector("div").appendChild(img);
+}
