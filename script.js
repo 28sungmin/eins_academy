e2c945b06a5a3c305fed362133fe228a0a114fbb
28sungmin <gooddays4001@naver.com>
2024-04-30 10:49:31 +0900

    header finish

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var no = 1;
 
 setInterval(function () {
   no++;
-  slide.style.backgroundImage = `url(./img/header_${no}.jpg)`;
-  if (no == 4) no = 1;
+  slide.style.backgroundImage = `url(./img/header2_${no}.jpeg)`;
+  if (no == 2) no = 0;
 }, 5000);
